bf0c0bfe2600156879dfc8f3e40e0ece715ba6e2
Rustam Khananov <rustam.khananov@careerist.com>
2024-04-03 08:13:26 +0300

    fix basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { createBrowserHistory } from 'history';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router history={createBrowserHistory()}>
+    <Router history={createBrowserHistory()} basename={window.location.pathname || ''}>
         <App />
     </Router>
   </React.StrictMode>
