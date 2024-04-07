ddc3f379b91b2c67c08edb7ed2d55e50c2b0f6e0
Лия Хананова <liahananova@Lias-MacBook-Air.local>
2024-04-07 23:32:10 +0200

    gradient and magazine

--- src/components/ContentContainer/components/projectsData.js
+++ src/components/ContentContainer/components/projectsData.js
@@ -1,34 +1,47 @@
-import firstImage from "./images/editorialSeasonsFirst.png"
-import secondImage from "./images/editorialSeasonsSecond.png"
-import thirdImage from "./images/editorialSeasonsThird.png"
-import coverImage from "./images/editorialSeasonsCover.png"
+import seasonsFirstImage from "./images/editorialSeasonsFirst.png"
+import seasonsSecondImage from "./images/editorialSeasonsSecond.png"
+import seasonsThirdImage from "./images/editorialSeasonsThird.png"
+import seasonsCoverImage from "./images/editorialSeasonsCover.png"
+// import fourthImage from "./images/editorialSeasonsFourth.png"
+import beatsCoverImage from "./images/editorialBeatsCover.png"
+import beatsFirstImage from "./images/editorialBeatsFirst.png"
+import beatsSecondImage from "./images/editorialBeatsSecond.png"
+import beatsThirdImage from "./images/editorialBeatsThird.png"
+
 
 export const projectsData = {
-        "Seasons of Life": {
+        "Project name 2": {
         pageSettings: {
             headerAlt: 'some alt text',
             designType: 'Cover Design',
             name: 'Seasons of Life',
-            headerImgSrc: coverImage,
-            explaining: '@poets_ua is an Instagram community, called to increase the awareness of modern Ukrainian poetry. Not only they are promoting it through the internet, but also publish books with chosen creations. This project was their second general book publishing, which was planned to be sold for gathering money for Ukrainian Armed Forces.The main tasks were to create a cover and the insight structure of the book.',
+            headerImgSrc: seasonsCoverImage,
+            explaining: '@poets_ua is an Instagram community, called to increase the awareness of modern Ukrainian poetry. Not only they are promoting it through the internet, but also publish books with chosen creations. This project was their second general book publishing, which was planned to be sold to gather money for the Ukrainian Armed Forces. The main tasks were to create a cover and the insight structure of the book.',
             body: [
                 {
                     text: 'To represent the main topics of the book I created two concepts. The FIrst one was based on one-line illustrations, connected to the rout of a person throughout life. The second one had a more abstract approach, based on form and color.',
-                    imageSrc: firstImage,
+                    imageSrc: seasonsFirstImage,
                     isImageFirst: true,
                 },
                 {
                     text: 'The choice of fonts was based on two major aspects. First of all, they needed to combine the feeling of poetry and the topic of poetry and be modern and strict enough. Secondly, it had to work with Cyrillic.',
-                    imageSrc: secondImage,
+                    imageSrc: seasonsSecondImage,
                     isImageFirst: false,
 
                 },
                 {
                     text: 'Illustrations were called to show the connection between all life events and to represent person’s feelings in minimalistic visual manner',
-                    imageSrc: thirdImage,
+                    imageSrc: seasonsThirdImage,
                     isImageFirst: true,
 
                 },
+
+                // {
+                //     text: '',
+                //     imageSrc: fourthImage,
+                //     isImageFirst: true,
+
+                // },
                 // {
                 //     text: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent.',
                 //     imageSrc: 'https://picsum.photos/400?random=5',
@@ -37,12 +50,12 @@ export const projectsData = {
             ]    
         },
         imageButtonSettings: {
-            imageWidth: '20vh',
-            imageHeight: '30vh',
+            imageWidth: '40vh',
+            // imageHeight: '30vh',
             imageAlt: 'some title',
-            imageLeft: 0,
+            imageLeft: '150px',
             imageTop: '100px',
-            imageSrc: 'https://picsum.photos/400?random=2',
+            imageSrc: seasonsCoverImage,
             containerWidth: '300px'
         }
     },
@@ -50,24 +63,24 @@ export const projectsData = {
         pageSettings: {
             headerAlt: 'some alt text',
             designType: 'editorial',
-            name: 'Seasons of Life',
-            headerImgSrc: 'https://picsum.photos/400?random=1',
-            explaining: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent. ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent.',
+            name: 'Electronic Beats',
+            headerImgSrc: beatsCoverImage,
+            explaining: 'Electronic Beats is a German magazine published from 2005 to 2015. The publishing itself was sponsored by the T-Mobile company. The main focus of the magazine was music, which was shown through interviews with musicians and singers. However, it also showed some movie recommendations or other artistic activities. in this redesigned version not only i am rethinking the visual aspect of the magazine, but also providing a more understandable structure.',
             body: [
                 {
                     text: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent. ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent.',
-                    imageSrc: 'https://picsum.photos/400?random=2',
+                    imageSrc: beatsFirstImage,
                     isImageFirst: true,
                 },
                 {
                     text: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent.',
-                    imageSrc: 'https://picsum.photos/400?random=3',
+                    imageSrc: beatsSecondImage,
                     isImageFirst: false,
 
                 },
                 {
                     text: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exent.',
-                    imageSrc: 'https://picsum.photos/400?random=4',
+                    imageSrc: beatsThirdImage,
                     isImageFirst: true,
 
                 },
@@ -79,12 +92,12 @@ export const projectsData = {
             ]    
         },
         imageButtonSettings: {
-            imageWidth: '200px',
-            imageHeight: '200px',
+            imageWidth: '60vh',
+            // imageHeight: '200px',
             imageAlt: 'some title',
             imageLeft: '100px',
-            imageTop: '0px',
-            imageSrc: 'https://picsum.photos/400?random=1',
+            imageTop: '300px',
+            imageSrc: beatsCoverImage,
             containerWidth: '100px'
         }
     },
